5ba55efed55963b2cdc99d7fa4fd161c4ffb11ec
Mike <levinmk23@gmail.com>
2026-03-03 17:41:54 +0300

    fix deps

--- commander/src/main/java/hse/java/commander/MainController.java
+++ commander/src/main/java/hse/java/commander/MainController.java
@@ -4,27 +4,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 
+import java.nio.file.Path;
+
 public class MainController {
-
-    @FXML
-    public Button move;
-
-    public void initialize() {
-        move.setOnMouseClicked(event -> {
-
-        });
-        System.out.println(System.getProperty("user.home"));
-       left.getItems().add("Kek");
-
-       left.setOnMouseClicked(event -> {
-           if (event.getClickCount() == 2) {
-               int index = left.getSelectionModel().getSelectedIndex();
-               if (index >= 0) {
-                   left.getItems().set(index, "clicked");
-               }
-           }
-       });
-   }
 
     @FXML
     public ListView<String> left;
@@ -32,5 +14,32 @@ public class MainController {
     @FXML
     public ListView<String> right;
 
+    @FXML
+    public Button move;
 
+    private Path leftDir;
+    private Path rightDir;
+
+    // for testing
+    public void setInitialDirs(Path leftStart, Path rightStart) {
+        this.leftDir = leftStart;
+        this.rightDir = rightStart;
+    }
+
+    public void initialize() {
+        move.setOnMouseClicked(event -> {
+
+        });
+        System.out.println(System.getProperty("user.home"));
+        left.getItems().add("Kek");
+
+        left.setOnMouseClicked(event -> {
+            if (event.getClickCount() == 2) {
+                int index = left.getSelectionModel().getSelectedIndex();
+                if (index >= 0) {
+                    left.getItems().set(index, "clicked");
+                }
+            }
+        });
+    }
 }
