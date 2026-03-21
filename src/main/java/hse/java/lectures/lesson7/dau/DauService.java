9bacf637b0e65b54e2769df972b1c5663dd32875
Mike <levinmk23@gmail.com>
2026-03-21 12:49:51 +0300

    lesson 8 task

--- src/main/java/hse/java/lectures/lesson7/dau/DauService.java
+++ src/main/java/hse/java/lectures/lesson7/dau/DauService.java
@@ -1,12 +1,13 @@
 package hse.java.lectures.lesson7.dau;
 
+import java.util.List;
 import java.util.Map;
 
 public interface DauService {
 
     void postEvent(Event event);
 
-    Map<Integer, Long> getDauStatistics();
+    Map<Integer, Long> getDauStatistics(List<Integer> authorIds);
 
     Long getAuthorDauStatistics(int authorId);
 
